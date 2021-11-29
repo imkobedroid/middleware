df21d89d1566f8e0cd5cc60e5777825f2a285fbc
dongshihong <imkobedroid@gmail.com>
2021-11-29 15:56:11 +0800

    test

--- Compiler/src/main/java/com/dragonest/pocket/compiler/TestProcessor.java
+++ Compiler/src/main/java/com/dragonest/pocket/compiler/TestProcessor.java
@@ -7,7 +7,6 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
-import java.lang.reflect.Modifier;
 import java.util.Collections;
 import java.util.Set;
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # middleware
+
+
+包含了（gradle插件开发、AOP、运行时注解）等开发代码实践
